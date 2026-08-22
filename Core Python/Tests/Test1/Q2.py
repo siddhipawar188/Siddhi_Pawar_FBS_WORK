c39f42d452f11f8cce4a416dc27baf8c8f9361e1
siddhipawar188 <siddhipawar079@gmail.com>
2026-08-22 15:34:40 +0530

    Delete Core Python/Tests/Test1 directory

--- Core Python/Tests/Test1/Q2.py
+++
@@ -1,8 +0,0 @@
-# WAp to calculate simple interest based on principle ,rate and time 
-
-p = int(input('Enter a principle:'))
-r = int(input('Enter a rate:'))
-t = int(input('Enter a time:'))
-
-SI = ((p * r * t)/100)
-print('Simple interest:',SI)
--- Core Python/Tests/Test1/Q3.py
+++
@@ -1,8 +0,0 @@
-# WAP to accept distance in km and convert it into meters and centimeters both.
-
-distance = int(input('Enter a distance in km:'))
-
-meter = distance *1000
-centimeter = meter * 100
-print('meter:',meter)
-print('centimeter',centimeter)
